616460a090c6e8273923a22007bd43ab6082e7e0
issac <20151313046@cqu.edu.cn>
2016-07-20 10:37:35 +0800

    add blue print

---
+++ indoorlocation/forms.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+from flask_wtf import Form
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Length
+
+
+# 定义登录表单
+class LoginForm(Form):
+    username = StringField(u'账号', validators=[DataRequired(), Length(1,20)])
+    password = PasswordField(u'密码', validators=[DataRequired(), Length(1,20)])
+    submit = SubmitField(u'登 录')
+
+
+# 定义管理员账户修改密码表单
+class ResetPassword(Form):
+    old_password = PasswordField(u'原始密码', validators=[DataRequired(), Length(1,20)])
+    new_password1 = PasswordField(u'新密码', validators=[DataRequired(), Length(1,20)])
+    new_password2 = PasswordField(u'确认新密码', validators=[DataRequired(), Length(1,20)])
+    submit = SubmitField(u'确 认 修 改')
+
+
+#新增用户表单
+class AddUser(Form):
+    username = StringField(u'登录账号', validators=[DataRequired(), Length(1,20)])
+    password = PasswordField(u'登录密码', validators=[DataRequired(), Length(1,20)])
+    realname = StringField(u'用户姓名', validators=[DataRequired(),Length(1,5)])
+    submit = SubmitField(u'提 交')
---
+++ indoorlocation/views/login.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+from flask import Flask, Blueprint, render_template
+
+from indoorlocation.forms import *
+
+main = Blueprint('login', __name__)
+
+
+# 管理员登录接口
+@main.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',form=form)
