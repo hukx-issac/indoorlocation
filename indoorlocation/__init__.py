e2c6f52ee0190fd0709b3184233346705c81b591
issac <20151313046@cqu.edu.cn>
2016-07-20 14:24:48 +0800

    __init__

--- indoorlocation/__init__.py
+++ indoorlocation/__init__.py
@@ -3,11 +3,9 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from indoorlocation.views import login
 
-# APP_CONFIG_FILE = ''
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
-# app.config.from_envvar('APP_CONFIG_FILE')
+app.config.from_object('config')    # 加载根目录配置
+app.config.from_pyfile('config.py')    # 从instance文件夹中加载配置
 bootstrap = Bootstrap(app)
 app.register_blueprint(login.main)
 
